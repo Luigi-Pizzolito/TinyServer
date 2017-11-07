452a3367d854fdd35211160b57ba34d0ce4afd7e
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2017-11-07 08:51:26 +0800

    Initial Commit

---
+++ TinyServer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TinyServer
+//
+//  Created by Luigi Pizzolito on 7/11/2017.
+//  Copyright © 2017 Luigi Pizzolito. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TinyServer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TinyServer
+//
+//  Created by Luigi Pizzolito on 7/11/2017.
+//  Copyright © 2017 Luigi Pizzolito. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
